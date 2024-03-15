fa473ead6537bad39cd4865173582333095b47ca
Kevin Schuren <k.schuren@student.avans.nl>
2024-03-15 08:54:10 +0100

    week 3-4

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,15 @@ var router = express.Router();
 const { db } = require("../services/database");
 
 /* GET users listing. */
-router.get('/', async function(req, res) {
-  let users = await db.collection('users').find().toArray();
-  res.json(users);
+router.get('/', async function (req, res) {
+  try {
+    let users = await db.collection('users').find().toArray();
+    console.log(users);
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({error: 'An error occurred while fetching users'});
+  }
 });
 
 router.post('/', function(req, res) {
